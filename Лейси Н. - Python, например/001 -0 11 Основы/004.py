93695e04b584e76290bac21ef115f8d62d3d67fc
eugen.stadnic <stadniceugeniu@gmail.com>
2022-02-07 19:35:38 +0200

    Add files

--- Лейси Н. - Python, например/001 -0 11 Основы/004.py
+++ Лейси Н. - Python, например/001 -0 11 Основы/004.py
@@ -3,4 +3,3 @@ num_1 = int(input('Input first number: '))
 num_2 = int(input('Input second number: '))
 answer = num_1 + num_2
 print('The total is:', answer)
-
--- Лейси Н. - Python, например/001 -0 11 Основы/005.py
+++ Лейси Н. - Python, например/001 -0 11 Основы/005.py
@@ -4,4 +4,3 @@ num_2 = int(input('Input second number: '))
 num_3 = int(input('Input third number: '))
 answer = (num_1 + num_2) * num_3
 print('The answer is:', answer)
-

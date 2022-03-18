673df07d4e06f1cf6de6354a65c28f30e9d1f22c
eugen.stadnic <stadniceugeniu@gmail.com>
2022-03-18 19:29:29 +0200

    edit 132

--- Лейси Н. - Python, например/124 - 132 Графический интерфейс Tkinter/132.py
+++ Лейси Н. - Python, например/124 - 132 Графический интерфейс Tkinter/132.py
@@ -42,28 +42,28 @@ label_name.pack()
 
 box_input_name = Entry()
 box_input_name['justify'] = 'center'
-box_input_name.pack()
+box_input_name.pack(pady=10)
 
 label_age = Label(text='Enter age')
 label_age.pack()
 
 box_input_age = Entry()
 box_input_age['justify'] = 'center'
-box_input_age.pack()
+box_input_age.pack(pady=10)
 
 but_1 = Button(text="Create new CSV",
                command=create_csv)
-but_1.pack()
+but_1.pack(pady=10)
 
 but_2 = Button(text="Add to CSV",
                command=save_csv)
-but_2.pack()
+but_2.pack(pady=10)
 
 but_3 = Button(text="open.csv",
                command=open_csv)
-but_3.pack()
+but_3.pack(pady=10)
 
 list_box = Listbox()
-list_box.pack()
+list_box.pack(pady=10)
 
 win.mainloop()
